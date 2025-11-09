09f6d23b87c6582c6f26d4da1b836b6a981cb533
astowner <the1samevl@gmail.com>
2025-11-09 13:37:30 +0200

    feat: move swagger from bootstrap file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,22 +3,14 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { setupSwagger } from './swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-
-  const config = new DocumentBuilder()
-    .setTitle('Quiz builder')
-    .setDescription('Quiz builder API description')
-    .setVersion('1.0')
-    .addTag('quiz-builder')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+  setupSwagger(app);
 
   await app.listen(process.env.PORT ?? 3000);
 }
