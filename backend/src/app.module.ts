336cafee66fe6fde8c189bc0a6b7f8f12a1f16cb
astowner <the1samevl@gmail.com>
2025-11-09 13:43:33 +0200

    feat: add docs config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { QuizModule } from './quiz/quiz.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import docsConfig from './config/docs.config';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot({ isGlobal: true }), QuizModule],
+  imports: [
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [docsConfig],
+    }),
+    QuizModule,
+  ],
 })
 export class AppModule {}
--- backend/src/swagger.ts
+++ backend/src/swagger.ts
@@ -1,12 +1,22 @@
 import { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export function setupSwagger(app: INestApplication): void {
+  const configService = app.get(ConfigService);
+
+  const swaggerConfig = {
+    title: configService.getOrThrow<string>('docs.title'),
+    description: configService.getOrThrow<string>('docs.description'),
+    version: configService.getOrThrow<string>('docs.version'),
+    tag: configService.getOrThrow<string>('docs.tag'),
+  };
+
   const config = new DocumentBuilder()
-    .setTitle('Quiz Builder')
-    .setDescription('Quiz Builder API description')
-    .setVersion('1.0')
-    .addTag('quiz-builder')
+    .setTitle(swaggerConfig.title)
+    .setDescription(swaggerConfig.description)
+    .setVersion(swaggerConfig.version)
+    .addTag(swaggerConfig.tag)
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
