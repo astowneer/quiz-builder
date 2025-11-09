7539308929ae8a5982381753cacd3c157f5461fe
astowner <the1samevl@gmail.com>
2025-11-09 14:45:50 +0200

    feat: change home page text position to center

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="h-full flex flex-col justify-center items-center space-y-5">
-      <h2 className="text-8xl font-bold drop-shadow-xl">
+    <main className="h-full flex flex-col justify-center items-center space-y-5 px-10">
+      <h2 className="max-md:text-6xl text-8xl max-w-2xl text-center font-bold drop-shadow-xl">
         Welcome to Quiz Builder
       </h2>
       <Link
-        className="text-4xl hover:text-white/60 duration-300 ease-in"
+        className="max-md:text-2xl text-4xl text-white/80 hover:text-white/60 duration-300 ease-in"
         href="/quizzes"
       >
         Go to your assets
